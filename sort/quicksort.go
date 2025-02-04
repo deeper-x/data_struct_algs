b2e07759803f809492bed901d95834abd9560489
justorius <albertodeprezzo@gmail.com>
2025-02-04 12:40:03 +0100

    quicksort refactoring

--- sort/quicksort.go
+++ sort/quicksort.go
@@ -5,41 +5,38 @@
 // worst time complexity: O(n^2)
 // average time complexity: O(n log n)
 // space complexity: O(log n)
-// author(s) [Taj](https://github.com/tjgurwara99)
 // see sort_test.go for a test implementation, test function TestQuickSort.
 
 package sort
 
-import "github.com/deeper-x/data_struct_algs/constraints"
+import (
+	"math/rand"
 
-func Partition[T constraints.Ordered](arr []T, low, high int) int {
-	index := low - 1
-	pivotElement := arr[high]
-	for i := low; i < high; i++ {
-		if arr[i] <= pivotElement {
-			index += 1
-			arr[index], arr[i] = arr[i], arr[index]
+	"github.com/deeper-x/data_struct_algs/constraints"
+)
+
+func Quicksort[T constraints.Ordered](arr []T) []T {
+	if len(arr) < 2 {
+		return arr
+	}
+
+	left, right := 0, len(arr)-1
+
+	pivot := rand.Int() % len(arr)
+
+	arr[pivot], arr[right] = arr[right], arr[pivot]
+
+	for i := range arr {
+		if arr[i] < arr[right] {
+			arr[left], arr[i] = arr[i], arr[left]
+			left++
 		}
 	}
-	arr[index+1], arr[high] = arr[high], arr[index+1]
-	return index + 1
-}
 
-// QuicksortRange Sorts the specified range within the array
-func QuicksortRange[T constraints.Ordered](arr []T, low, high int) {
-	if len(arr) <= 1 {
-		return
-	}
+	arr[left], arr[right] = arr[right], arr[left]
 
-	if low < high {
-		pivot := Partition(arr, low, high)
-		QuicksortRange(arr, low, pivot-1)
-		QuicksortRange(arr, pivot+1, high)
-	}
-}
+	Quicksort(arr[:left])
+	Quicksort(arr[left+1:])
 
-// Quicksort Sorts the entire array
-func Quicksort[T constraints.Ordered](arr []T) []T {
-	QuicksortRange(arr, 0, len(arr)-1)
 	return arr
 }
